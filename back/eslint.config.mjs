376201fa48abea52834874d727bd8d396212a90b
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2026-03-05 19:10:07 +0100

    feat: maldito seas prisma

--- back/eslint.config.mjs
+++ back/eslint.config.mjs
@@ -26,11 +26,18 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": 'warn',
+// Las que ya teníamos
+      'prettier/prettier': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      
+      //Convertir las comprobaciones estrictas de tipos (Prisma) en warnings
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
+      
+      '@typescript-eslint/require-await': 'warn',
     },
   },
 );
