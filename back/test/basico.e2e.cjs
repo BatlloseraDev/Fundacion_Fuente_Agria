8d37ae115320ff1a7a73ae293080fcd2f5156e1f
Diego <diegogarciaangel25@gmail.com>
2026-03-06 22:03:49 +0100

    feat: arreglo test back 2

--- back/test/basico.e2e.cjs
+++ back/test/basico.e2e.cjs
@@ -1,22 +1,21 @@
 const request = require('supertest');
 const express = require('express');
-const kleur = require('kleur');
 
 const app = express();
 app.get('/', (req, res) => res.status(200).send('OK'));
 
 async function runBasicoTests() {
-  console.log(kleur.blue('Tests: Básico Github Actions'));
+  console.log('Tests: Básico Github Actions');
 
   try {
     const res = await request(app).get('/');
 
     if (!res.status) {
-      throw new Error(kleur.red('El servidor no ha respondido nada'));
+      throw new Error('El servidor no ha respondido nada');
     }
 
-    console.log(kleur.green('Test básico superado'));
-    console.log(kleur.blue('Tests: Básico Github Actions OK\n'));
+    console.log('Test básico superado');
+    console.log('Tests: Básico Github Actions OK\n');
     
     process.exit(0); 
   } catch (error) {
